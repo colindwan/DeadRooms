5849f60af0b7b86a1c7892bd0b6d6c05057ee0a9
Colin Dwan <thedwans@gmail.com>
2011-07-24 13:58:51 -0400

    initial commit

---
+++ DeadRooms/Resources/Tiles/grass.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE tileset SYSTEM "http://mapeditor.org/dtd/1.0/map.dtd">
+<tileset name="grass_tight" tilewidth="40" tileheight="40">
+ <image source="grass_tight.png" width="256" height="128"/>
+</tileset>
---
+++ DeadRooms/Resources/Tiles/wood.tsx
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE tileset SYSTEM "http://mapeditor.org/dtd/1.0/map.dtd">
+<tileset name="wood" tilewidth="40" tileheight="40">
+ <image source="wood.png" width="128" height="128"/>
+ <tile id="0">
+  <properties>
+   <property name="walkable" value="-1"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="walkable" value="-1"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="walkable" value="-1"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="walkable" value="-1"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="walkable" value="-1"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="walkable" value="-1"/>
+  </properties>
+ </tile>
+</tileset>
